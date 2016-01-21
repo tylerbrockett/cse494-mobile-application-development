fe8017cd5fa02947b700d114572fea4660b2cd15
Tyler Brockett <tylerbrockett@gmail.com>
2016-01-20 22:17:20 -0700

    Lab 1 ready for submission

--- lab-1/lab_1_ios/lab-1-ios/MovieDescription.swift
+++ lab-1/lab_1_ios/lab-1-ios/MovieDescription.swift
@@ -1,10 +1,30 @@
-//
-//  MovieDescription.swift
-//  lab-1-ios
-//
-//  Created by Tyler Brockett on 1/16/16.
-//  Copyright © 2016 Tyler Brockett. All rights reserved.
-//
+/*
+ * @author Tyler Brockett
+ * @project CSE 494 Lab 1 - iOS
+ * @version January 2016
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tyler Brockett
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 import Foundation
 
@@ -84,18 +104,5 @@ class MovieDescription{
     
     func getPlot() -> String {
         return self.plot
-    }
-    
-    func toJsonString() -> String {
-        var jsonStr = "";
-        let dict = ["Title": self.title, "Year": self.year, "Rated": self.rated, "Released": self.released,
-                    "Runtime": self.runtime, "Genre": self.genre, "Actors": self.actors, "Plot": self.plot]
-        do {
-            let jsonData = try NSJSONSerialization.dataWithJSONObject(dict, options: NSJSONWritingOptions.PrettyPrinted)
-            jsonStr = NSString(data: jsonData, encoding: NSUTF8StringEncoding)! as String
-        } catch let error as NSError {
-            print(error)
-        }
-        return jsonStr
     }
 }
