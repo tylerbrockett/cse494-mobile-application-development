7d86d594c9d624a5474f7966a392beb8cf59f991
Tyler Brockett <tylerbrockett@gmail.com>
2016-02-11 16:05:05 -0700

    lab-4 Initialized

---
+++ lab-4/lab-4/lab-4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab-4
+//
+//  Created by tkbrocke on 2/11/16.
+//  Copyright © 2016 edu.asu.bscs.tkbrocke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
